ef2febb91bceb5ad8f437f2fcc26634e1f3cb1d0
Sheep-IK-LT <yik418@naver.com>
2023-10-12 11:58:29 +0900

    누워서 드르렁 코골기

--- control_boy.py
+++ control_boy.py
@@ -1,36 +1,10 @@
 from pico2d import *
-import random
+
+from grass import Grass
+from boy import Boy
 
 
 # Game object class here
-
-class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
-
-    def update(self):
-        pass
-
-
-class Boy:
-    def __init__(self):
-        self.x, self.y = 400, 90
-        self.frame = 0
-        self.dir = 0
-        self.action = 3
-        self.image = load_image('animation_sheet.png')
-
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-
-    def handle_event(self, event):
-        pass
-
-    def draw(self):
-        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
 
 
 def handle_events():
@@ -84,6 +58,6 @@ while running:
     handle_events()
     update_world()
     render_world()
-    delay(0.1)
+    delay(0.01)
 # finalization code
 close_canvas()
